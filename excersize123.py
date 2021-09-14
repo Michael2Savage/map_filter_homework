357e08410f1f3fb152563878a6a74ce7f35e7001
btwist540@yahoo.com <btwist540@yahoo.com>
2021-09-14 10:32:35 -0400

    Edits

--- excersize123.py
+++ excersize123.py
@@ -3,7 +3,7 @@ print("Excersize 1")
 
 places = [" ","Argentina", " ", "San Diego","","  ","","Boston","New York"]
 
-no_spaces = list(filter(lambda x: True if x.strip() == x else False, places))
+no_spaces = list(filter(lambda x: True if x.strip() else False, places))
 while("" in no_spaces):
     no_spaces.remove("")
 print(no_spaces)
@@ -16,15 +16,15 @@ print("Excersize 2")
 
 authors = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
 
-def seperate_name():
-    new_list = []
-    for author in authors:
-        split_name =  author.split()
-        last_name = split_name[-1]
-        new_list.append(last_name)
-    return new_list
+def seperate_name(author):
+    split_name =  author.split()
+    last_name = split_name[-1].lower()
+    return last_name
 
-print(seperate_name())
+authors.sort(key = seperate_name)
+print(authors)
+
+
     
 #Could not for the life of me get the sort or sorted function to work on the new_list variable OR the seperate_name() func.
 
@@ -35,5 +35,8 @@ print("Excersize 3")
 
 places = [('Nashua',32),("Boston",12),("Los Angelos",44),("Miami",29)]
 
-celcius_convert = list(map(lambda x:  y*(9/5) + 32, places))
-print(celcius_convert)
+#celcius_convert = list(map(lambda x:  x[-1]*(9/5) + 32, places))
+#print(celcius_convert)
+
+ans = list(map(lambda x : (x[0], (9/5)*x[1]+32), places))
+print(ans)
